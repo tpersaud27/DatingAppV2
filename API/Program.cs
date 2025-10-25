10f5a56ccd291d93d6c33570aef88a06fa6bed63
Tommy Persaud <tpersaud27@gmail.com>
2025-10-25 15:05:49 -0400

    chore: apply .gitignore cleanup

--- API/Program.cs
+++ API/Program.cs
@@ -9,15 +9,6 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
