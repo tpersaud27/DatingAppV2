5bbc22bb1043a3ac24042677be0a19739e599cd6
Tommy Persaud <tpersaud27@gmail.com>
2025-11-04 12:09:30 -0500

    Initial Http Request

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('Client');
+export class App implements OnInit {
+  // Note: This is a new way of dependency injection
+  private http = inject(HttpClient);
+
+  protected readonly title = signal('Dating App');
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => console.log('Completed the http request'),
+    });
+  }
 }
