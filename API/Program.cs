9f8f6d30dd42308adb3fbb136b627df33bc906f4
Tommy Persaud <tpersaud27@gmail.com>
2025-11-04 12:24:01 -0500

    Adding CORS configuration

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Addings CORS configuration
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// This is known as middleware
+
+app.UseCors(options =>
+    options
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200")
+);
 app.MapControllers();
 
 app.Run();
