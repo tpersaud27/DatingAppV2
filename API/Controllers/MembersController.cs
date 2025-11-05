96de2104e3ac6edf28ff9b013613027536083f34
Tommy Persaud <tpersaud27@gmail.com>
2025-11-05 18:24:03 -0500

    Updating members controller to use members repository

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,28 +1,25 @@
-using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [Authorize]
-    public class MembersController(AppDbContext context) : BaseApiController
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         // Action Result allows us to return a HTTP Response
         // Task allows us to return a async operation
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-            var Members = await context.Users.ToListAsync();
-
-            return Members;
+            return Ok(await memberRepository.GetMembersAsync());
         }
 
         [HttpGet("{id}")] //api/members/id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
+        public async Task<ActionResult<Member>> GetMember(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
 
             // If the member is not found we return a 404 http response
             if (member == null)
@@ -31,6 +28,12 @@ namespace API.Controllers
             }
 
             return member;
+        }
+
+        [HttpGet("{id}/photos")] // api/members/{id}/photos
+        public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
+        {
+            return Ok(await memberRepository.GetPhotosForMemberAsync(id));
         }
     }
 }
