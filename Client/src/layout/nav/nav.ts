f49f5acf345fd0748593ac671b686d622e29eb36
Tommy Persaud <tpersaud27@gmail.com>
2025-11-05 19:14:02 -0500

    Adding nav bar

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {
-
-}
+export class Nav {}
