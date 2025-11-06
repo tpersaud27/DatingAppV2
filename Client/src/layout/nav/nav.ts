6093e2475dc17f80e4cdb70c4e9af9b9e7704760
Tommy Persaud <tpersaud27@gmail.com>
2025-11-05 19:26:05 -0500

    Adding reactive form for navbar login

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {}
+export class Nav {
+  private fb = inject(FormBuilder);
+
+  public loginForm!: FormGroup;
+
+  public ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  public login(): void {
+    if (this.loginForm.invalid) return;
+
+    const creds = this.loginForm.value;
+    console.log('Logging in with:', creds);
+  }
+}
