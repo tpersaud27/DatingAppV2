c127da182b73894da7f07b3197f104fc9572f35f
Tommy Persaud <tpersaud27@gmail.com>
2025-11-17 18:14:10 -0500

    Getting member details from route snapshot

--- Client/src/features/members/member-detailed/member-detailed.ts
+++ Client/src/features/members/member-detailed/member-detailed.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MemberService } from '../../../core/services/member-service';
+import { ActivatedRoute } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import { Member } from '../../../Types/Member';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css',
 })
-export class MemberDetailed {
+export class MemberDetailed implements OnInit {
+  private memberService = inject(MemberService);
+  private route = inject(ActivatedRoute);
 
+  public member$?: Observable<Member | null>;
+
+  ngOnInit() {
+    this.member$ = this.loadMember();
+  }
+
+  // Getting the member id to load their details
+  public loadMember(): Observable<Member | null> {
+    // This is will get us the root parameter that has the key of id
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id) {
+      return of(null);
+    } else {
+      return this.memberService.getMember(id);
+    }
+  }
 }
