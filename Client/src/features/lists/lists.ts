e121dd3cbd510f988f21839c5c0740074ff596a6
Tommy Persaud <tpersaud27@gmail.com>
2025-12-22 18:30:36 -0500

    Added option to toggle like on lists page

--- Client/src/features/lists/lists.ts
+++ Client/src/features/lists/lists.ts
@@ -6,27 +6,33 @@ import { LikesPredicate } from '../../Types/Like';
 import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-lists',
-  imports: [MatTabsModule, MatCardModule, RouterModule, AgePipe],
+  imports: [MatTabsModule, MatCardModule, RouterModule, AgePipe, MatIconModule, MatButtonModule],
   templateUrl: './lists.html',
   styleUrl: './lists.css',
 })
 export class Lists implements OnInit {
   // Dependencies
+
   private likesService = inject(LikesServices);
 
   // State
+
   public members = signal<Member[]>([]);
   public predicate: LikesPredicate = LikesPredicate.Liked;
 
   // LifeCycle
+
   public ngOnInit(): void {
     this.loadLikes();
   }
 
   // Actions
+
   public onTabChange(event: MatTabChangeEvent): void {
     switch (event.index) {
       case 0:
@@ -43,7 +49,25 @@ export class Lists implements OnInit {
     this.loadLikes();
   }
 
+  public onLike(event: MouseEvent, memberId: string): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.likesService.toggleLike(memberId).subscribe({
+      next: () => {
+        this.likesService.getLikeIds().subscribe();
+        this.loadLikes();
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
   // Methods
+  public hasLiked(memberId: string): boolean {
+    return this.likesService.likedIds().includes(memberId);
+  }
+
   public loadLikes(): void {
     this.likesService.getLikes(this.predicate).subscribe({
       next: (members) => {
