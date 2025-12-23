170546f89155f647bb55e8bf2f7cbfa632d12756
Tommy Persaud <tpersaud27@gmail.com>
2025-12-23 02:58:26 -0500

    Update init service to have the currentUserId passed to the messageSocketService

--- Client/src/core/services/init-service.ts
+++ Client/src/core/services/init-service.ts
@@ -1,7 +1,9 @@
+import { MessagesSocketService } from './messages-socket-service';
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
 import { Observable, of } from 'rxjs';
 import { LikesServices } from './likes-services';
+import { User } from '../../Types/User';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +11,19 @@ import { LikesServices } from './likes-services';
 export class InitService {
   private accountService = inject(AccountService);
   private likesService = inject(LikesServices);
+  private messagesSocketService = inject(MessagesSocketService);
 
   public init(): Observable<null> {
     const userString = localStorage.getItem('user');
     if (!userString) return of(null);
 
     // If we have the user
-    const user = JSON.parse(userString);
+    const user: User = JSON.parse(userString);
     this.accountService.currentUser.set(user);
+    const currentUser = this.accountService.currentUser();
+    if (currentUser) {
+      this.messagesSocketService.connect(currentUser.id);
+    }
     this.likesService.getLikeIds().subscribe();
 
     return of(null);
