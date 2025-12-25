fceeb677560cce55f0b72929bede5a45351dd6d3
Tommy Persaud <tpersaud27@gmail.com>
2025-12-25 01:54:27 -0500

    UpdateReadME

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -4,15 +4,24 @@ namespace API.Interfaces
 {
     public interface IMessageRepository
     {
+        // Creates a conversations (1 to 1 communication thread) between two users
         Task<Conversation> GetOrCreateConversationAsync(string userId, string otherUserId);
+
+        // Persists message to DB
         Task<Message> AddMessageAsync(Message message);
+
+        // Returns messages in a conversation
         Task<IReadOnlyList<Message>> GetMessagesAsync(
             string conversationId,
             string currentUserId,
             int take,
             DateTime? before
         );
+
+        // Returns all user conversations
         Task<IReadOnlyList<Conversation>> GetUserConversationsAsync(string userId);
+
+        // Saves DB changes
         Task<bool> SaveAllAsync();
     }
 }
