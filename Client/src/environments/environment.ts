288a821db2fda5ca36d11635e442536eaf60c03f
Tommy Persaud <tpersaud27@gmail.com>
2026-01-24 16:05:42 -0500

    Recreate environment.ts file

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
-  wsUrl: 'wss/',
-  cognito: {
-    domain: 'https://dating-app-v2-auth.auth.us-east-1.amazoncognito.com',
-    clientId: '7ie27nb65tl0t77dcp3ee4ln4h',
-    redirectUri: 'http://localhost:4200/auth/callback',
-    logoutUri: 'http://localhost:4200/',
-    scopes: ['openid', 'email'],
-  },
 };
