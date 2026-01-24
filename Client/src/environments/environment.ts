8cde45e1dffc50feada93bb68c1f9e5f14c8267b
Tommy Persaud <tpersaud27@gmail.com>
2026-01-24 16:20:53 -0500

    Add missing cognito info to environment file

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  cognito: {
+    domain: '',
+    clientId: '',
+    redirectUri: '',
+    logoutUri: '',
+    scopes: ['openid', 'email'],
+  },
 };
