2c4d3578182612a1b892ae69a72cb4402d02e5a2
Tommy Persaud <tpersaud27@gmail.com>
2026-01-27 11:17:12 -0500

    Update exception middleware response

--- API/Errors/ApiException.cs
+++ API/Errors/ApiException.cs
@@ -1,9 +1,10 @@
 namespace API.Errors
 {
-    public class ApiException(int statusCode, string message, string? details)
+    public class ApiException()
     {
-        public int StatusCode { get; set; } = statusCode;
-        public string Message { get; set; } = message;
-        public string? Details { get; set; } = details;
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public string? Details { get; set; }
+        public string? TraceId { get; set; }
     }
 }
